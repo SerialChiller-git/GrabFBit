ba66029ed638ab6f4e488fcd2420e0692bd8cfd1
Serial CHiller <67603638+Wise-Dude@users.noreply.github.com>
2025-05-20 22:26:29 +0600

    instagram also added

--- video-downloader-react/src/components/VideoDownloader.tsx
+++ video-downloader-react/src/components/VideoDownloader.tsx
@@ -102,7 +102,7 @@ const VideoDownloader: React.FC = () => {
     return (
         <div className='main-container'>
             <h1 className='title'>GrabFBit</h1>
-            <h3 className='subtitle'>Download any facebook video using the link</h3>
+            <h3 className='subtitle'>Download any facebook or instagram video using the link</h3>
             <form className='submitForm' onSubmit={handleSubmit}>
                 <input
                     type="text"
